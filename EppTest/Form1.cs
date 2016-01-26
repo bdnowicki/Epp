c760770905a04518efd05125a32d48ec32bc96f6
bdnowicki <b.nowicki@promoball.pl>
2016-01-26 09:54:22 +0100

    EppTest

---
+++ EppTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EppParser;
+using System.IO;
+using EppParser.Classes;
+
+namespace EppTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EppFile epp = new EppFile();
+            epp.LoadEpp(new FileStream(@"C:\udost\test.epp", FileMode.Open));
+            textBox1.Text = epp.GetString();
+            File.WriteAllText(@"C:\udost\test2.epp", epp.GetString(), Encoding.GetEncoding(1250));
+        }
+    }
+}
---
+++ EppTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EppTest
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+}
